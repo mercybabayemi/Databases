844afef5539ab509692d1efb9c47f3f2a21000a1
mercybabayemi <mercyjanet013@gmail.com>
2025-03-20 08:16:30 +0100

    update database

--- SQL/chapter_two/march2025/cape_codd_ch02_rq.sql
+++ SQL/chapter_two/march2025/cape_codd_ch02_rq.sql
@@ -60,3 +60,50 @@ FROM inventory;
 SELECT DISTINCT SKU, SKU_Description LIKE '___d%'
 FROM inventory;
 
+SELECT SUM(QuantityOnHand) AS QuantitySum
+FROM inventory;
+
+SELECT SUM(WarehouseID) AS WareIDSum,
+AVG(WarehouseID) AS WareIDAverage,
+MIN(WarehouseID) AS WareIDMin,
+MAX(WarehouseID) AS WareIDMax,
+SUM(QuantityOnHand) AS QuantityHandSum,
+AVG(QuantityOnHand) AS QuantityHandAverage,
+MIN(QuantityOnHand) AS QuantityHandMin,
+MAX(QuantityOnHand) AS QuantityHandMax,
+SUM(QuantityOnOrder) AS QuantityOrderSum,
+AVG(QuantityOnOrder) AS QuantityOrderAverage,
+MIN(QuantityOnOrder) AS QuantityOrderMin,
+MAX(QuantityOnOrder) AS QuantityOrderMax
+FROM inventory;
+
+SELECT COUNT(*) AS InventoryNumberOfRows
+FROM inventory;
+
+/* Difference between SQL Count and Sum
+The SQL built-in function count and sum sounds similar, however, they produce different result. 
+The count function counts the number of rows in a column while the sum function calculates the total of all values in a column*/ 
+
+
+SELECT WarehouseID, 
+SUM(QuantityOnHand) AS TotalItemsOnHand
+FROM inventory
+GROUP BY WarehouseID
+ORDER BY  TotalItemsOnHand DESC;
+
+SELECT WarehouseID, 
+SUM(QuantityOnHand) AS TotalItemsOnHandLT3
+FROM inventory
+WHERE QuantityOnHand < 3
+GROUP BY WarehouseID
+ORDER BY  TotalItemsOnHandLT3 DESC;
+
+SELECT WarehouseID, 
+SUM(QuantityOnHand) AS TotalItemsOnHandLT3
+FROM inventory
+WHERE QuantityOnHand < 3
+GROUP BY WarehouseID
+HAVING COUNT(SKU) <	 2
+ORDER BY  TotalItemsOnHandLT3 DESC;
+
+
